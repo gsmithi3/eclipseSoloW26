55b3032b846c8226531b33b1afa8a7f36f8298bc
George Smith <georgedsmithii@gmail.com>
2026-02-16 00:09:34 -0500

    Added second println statement

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -1,5 +1,9 @@
 package eclipseSoloProjA;
 
 public class HelloWorld {
-
+	public static void main(String args[]) {
+		System.out.println("Hello Github Project");
+		System.out.println("next line");
+		
+	}
 }

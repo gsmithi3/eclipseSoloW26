89072534099ee76b83193816ede3e662ba00fb5b
George Smith <georgedsmithii@gmail.com>
2026-02-16 00:10:41 -0500

    added new line

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("Hello Github Project");
 		System.out.println("next line");
+		System.out.println("");
 		
 	}
 }

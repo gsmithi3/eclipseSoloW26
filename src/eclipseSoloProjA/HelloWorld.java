6ded6bb2c5660e8d5f9caa71942241e322ae5298
George Smith <georgedsmithii@gmail.com>
2026-02-16 00:50:36 -0500

    first push from P2

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("Hello Github Project");
 		System.out.println("next line");
-		
+		System.out.println("Programmer 2 did this");
 	}
 }
